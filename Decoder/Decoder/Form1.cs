ba1b21e2387de671e4e2211966ece4b7ff829bff
Toschyk <toschyk@icloud.com>
2023-09-19 08:51:19 +0700

    1.1

---
+++ Decoder/Decoder/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+
+namespace Decoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string str;
+        byte[] key;
+        byte[] te;
+        byte[] cipherbytes;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader file = new StreamReader("my_file.txt");
+            file.Close();
+            te = new Byte[str.Length];
+            te = Encoding.Default.GetBytes(str);
+            SymmetricAlgorithm sa2 = TripleDES.Create();
+            sa2.Key = key;
+            sa2.Mode = CipherMode.ECB;
+            sa2.Padding = PaddingMode.PKCS7;
+            MemoryStream ms2 = new MemoryStream(te);
+            CryptoStream cs2 = new CryptoStream(ms2, sa2.CreateDecryptor(), CryptoStreamMode.Read);
+            byte[] plainbytes2 = new Byte[te.Length];
+            cs2.Read(plainbytes2, 0, te.Length);
+            cs2.Close();
+            ms2.Close();
+            textBox1.Text = Encoding.Default.GetString(plainbytes2);
+        }
+    }
+}
---
+++ DecryptionV2/DecryptionV2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("DecryptionV2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DecryptionV2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("974561c5-3461-4329-a590-ff53d401db49")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
